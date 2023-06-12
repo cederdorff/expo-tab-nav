4b9c8f88219b01eddb6fded10c014a661ed225bf
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-06-13 00:00:37 +0200

    Styles with the green you know...

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -3,19 +3,31 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#264c59"
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                    fontWeight: "bold"
+                },
+                tabBarStyle: {
+                    backgroundColor: "#264c59"
+                }
+            }}>
             <Tabs.Screen
                 name="home"
                 options={{
                     title: "Home",
-                    tabBarIcon: () => <Ionicons name="home-outline" size={24} />
+                    tabBarIcon: () => <Ionicons name="home-outline" size={24} color="white" />
                 }}
             />
             <Tabs.Screen
                 name="second"
                 options={{
                     title: "Tab two",
-                    tabBarIcon: () => <Ionicons name="albums-outline" size={24} />
+                    tabBarIcon: () => <Ionicons name="albums-outline" size={24} color="white" />
                 }}
             />
         </Tabs>

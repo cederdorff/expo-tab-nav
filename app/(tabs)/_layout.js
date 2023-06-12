b9d43bb9602ed9d62a67ab9ba345e46a1dd8c213
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-06-12 23:51:27 +0200

    import clean up

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,6 +1,5 @@
-import { Tabs } from "expo-router";
-import { Text } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
 export default function TabLayout() {
     return (
